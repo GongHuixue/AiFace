a505751ac6cfb23ee7b04d4ff44dc0dc398fc894
GongHuixue <gonghuixue@hotmail.com>
2018-10-04 08:20:51 +0800

    add detected activity

--- app/src/main/java/android/com/aiface/ui/activity/MeetingRegActivity.java
+++ app/src/main/java/android/com/aiface/ui/activity/MeetingRegActivity.java
@@ -1,13 +1,18 @@
 package android.com.aiface.ui.activity;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.com.aiface.R;
+import android.com.aiface.baidu.utils.ImageSaveUtil;
 import android.com.aiface.database.bean.MeetingFace;
 import android.com.aiface.ui.base.BaseActivity;
 import android.com.aiface.ui.presenter.MeetingPresenter;
 import android.com.aiface.ui.view.IMeetingView;
 import android.com.aiface.utils.DateTime;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
@@ -48,6 +53,9 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
     private MeetingFace mMeetingFace = new MeetingFace();
     boolean meetingPassed = false;
 
+    private String facePath;
+    private Bitmap faceBmp;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_meeting_reg;
@@ -58,24 +66,24 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
 
         getMeetingInformation();
 
-        if(meetingFaceList.size() > 0) {
+        if (meetingFaceList.size() > 0) {
             mMeetingFace = meetingFaceList.get(0);
 
-            if(mMeetingFace.getMeetingTime() < mDateTime.getSystemTime()) {
+            if (mMeetingFace.getMeetingTime() < mDateTime.getSystemTime()) {
                 meetingPassed = true;
             }
 
             updateDateTimeLL(meetingPassed);
 
-            if(meetingPassed) {
+            if (meetingPassed) {
                 mToastInstance.showLongToast("没有有效的会议，请创建新的会议");
                 // need create a new meeting
-            }else {
+            } else {
                 updateEtMeetingInfo();
                 //update meeting name/time/addr
             }
             Log.d(TAG, "Show the latest meeting info");
-        }else {
+        } else {
             updateDateTimeLL(true);
             mToastInstance.showLongToast("没有有效的会议，请创建新的会议");
             Log.d(TAG, "No any meeting information");
@@ -90,47 +98,47 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
         tv_title.setText(R.string.meeting_title);
 
 
-        mMeetingNamell = (LinearLayout)findViewById(R.id.te_meeting_name);
-        TextView tvMeetingName = (TextView)mMeetingNamell.findViewById(R.id.tv_name);
-        etMeetingName = (EditText)mMeetingNamell.findViewById(R.id.et_name);
+        mMeetingNamell = (LinearLayout) findViewById(R.id.te_meeting_name);
+        TextView tvMeetingName = (TextView) mMeetingNamell.findViewById(R.id.tv_name);
+        etMeetingName = (EditText) mMeetingNamell.findViewById(R.id.et_name);
         tvMeetingName.setText(R.string.meeting_name);
 
-        mMeetingAddrll = (LinearLayout)findViewById(R.id.te_meeting_addr);
-        TextView tvMeetingAddr = (TextView)mMeetingAddrll.findViewById(R.id.tv_name);
-        etMeetingAddr = (EditText)mMeetingAddrll.findViewById(R.id.et_name);
+        mMeetingAddrll = (LinearLayout) findViewById(R.id.te_meeting_addr);
+        TextView tvMeetingAddr = (TextView) mMeetingAddrll.findViewById(R.id.tv_name);
+        etMeetingAddr = (EditText) mMeetingAddrll.findViewById(R.id.et_name);
         tvMeetingAddr.setText(R.string.meeting_addr);
 
-        mMeetingTimell = (LinearLayout)findViewById(R.id.te_meeting_time);
-        TextView tvMeetingTime = (TextView)mMeetingTimell.findViewById(R.id.tv_name);
+        mMeetingTimell = (LinearLayout) findViewById(R.id.te_meeting_time);
+        TextView tvMeetingTime = (TextView) mMeetingTimell.findViewById(R.id.tv_name);
         tvMeetingTime.setText(R.string.meeting_time);
-        etMeetingTime = (EditText)mMeetingTimell.findViewById(R.id.et_name);
+        etMeetingTime = (EditText) mMeetingTimell.findViewById(R.id.et_name);
 
-        mSetMeetingTimell = (LinearLayout)findViewById(R.id.set_meeting_time);
-        TextView tvSetMeetingTime = (TextView)mSetMeetingTimell.findViewById(R.id.tv_name);
+        mSetMeetingTimell = (LinearLayout) findViewById(R.id.set_meeting_time);
+        TextView tvSetMeetingTime = (TextView) mSetMeetingTimell.findViewById(R.id.tv_name);
         tvSetMeetingTime.setText(R.string.meeting_time);
-        btn_date = (Button)mSetMeetingTimell.findViewById(R.id.btn_date);
-        btn_time = (Button)mSetMeetingTimell.findViewById(R.id.btn_time);
+        btn_date = (Button) mSetMeetingTimell.findViewById(R.id.btn_date);
+        btn_time = (Button) mSetMeetingTimell.findViewById(R.id.btn_time);
         btn_date.setOnClickListener(this);
         btn_time.setOnClickListener(this);
-        tv_date = (TextView)mSetMeetingTimell.findViewById(R.id.tv_date);
-        tv_time = (TextView)mSetMeetingTimell.findViewById(R.id.tv_time);
+        tv_date = (TextView) mSetMeetingTimell.findViewById(R.id.tv_date);
+        tv_time = (TextView) mSetMeetingTimell.findViewById(R.id.tv_time);
 
 
-        mParticipantName = (LinearLayout)findViewById(R.id.te_participant_name);
-        TextView tvParticipantName = (TextView)mParticipantName.findViewById(R.id.tv_name);
-        etParticipantName = (EditText)mParticipantName.findViewById(R.id.et_name);
+        mParticipantName = (LinearLayout) findViewById(R.id.te_participant_name);
+        TextView tvParticipantName = (TextView) mParticipantName.findViewById(R.id.tv_name);
+        etParticipantName = (EditText) mParticipantName.findViewById(R.id.et_name);
         tvParticipantName.setText(R.string.participant_name);
 
-        mParticipantPart = (LinearLayout)findViewById(R.id.te_participant_part);
-        TextView tvParticipantPart= (TextView)mParticipantPart.findViewById(R.id.tv_name);
-        etParticipantPart = (EditText)mParticipantPart.findViewById(R.id.et_name);
+        mParticipantPart = (LinearLayout) findViewById(R.id.te_participant_part);
+        TextView tvParticipantPart = (TextView) mParticipantPart.findViewById(R.id.tv_name);
+        etParticipantPart = (EditText) mParticipantPart.findViewById(R.id.et_name);
         tvParticipantPart.setText(R.string.participant_part);
 
-        mFaceImageRl = (RelativeLayout)findViewById(R.id.upload_face_rl);
-        faceIv = (ImageView)mFaceImageRl.findViewById(R.id.iv_face_image);
+        mFaceImageRl = (RelativeLayout) findViewById(R.id.upload_face_rl);
+        faceIv = (ImageView) mFaceImageRl.findViewById(R.id.iv_face_image);
         faceIv.setImageResource(R.drawable.face_default);
-        btnLocal = (Button)mFaceImageRl.findViewById(R.id.btn_from_local);
-        btnCamera = (Button)mFaceImageRl.findViewById(R.id.btn_from_camera);
+        btnLocal = (Button) mFaceImageRl.findViewById(R.id.btn_from_local);
+        btnCamera = (Button) mFaceImageRl.findViewById(R.id.btn_from_camera);
         btnLocal.setOnClickListener(this);
         btnCamera.setOnClickListener(this);
 
@@ -161,13 +169,51 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
                 showTimeDialog();
                 break;
             case R.id.btn_from_local:
+                uploadFromAlbum();
                 break;
             case R.id.btn_from_camera:
+                uploadFromCamera();
                 break;
             case R.id.btn_reg:
                 getMeetingTime();
                 registerMeetingInfo();
+                registerFaceImage(facePath, etParticipantName.getText().toString().trim());
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == REQUEST_CODE_DETECT_FACE) && (resultCode == Activity.RESULT_OK)) {
+            facePath = ImageSaveUtil.loadCameraBitmapPath(this, "face.jpg");
+            if (faceBmp != null) {
+                faceBmp.recycle();
+            }
+            faceBmp = ImageSaveUtil.loadCameraBitmap(this, facePath);
+            if (faceBmp != null) {
+                faceIv.setImageBitmap(faceBmp);
+            }
+        } else if ((requestCode == REQUEST_CODE_PICK_IMAGE) && (resultCode == Activity.RESULT_OK)) {
+            Uri uri = data.getData();
+            facePath = getFacePathFromURI(uri);
+
+            if (faceBmp != null) {
+                faceBmp.recycle();
+            }
+
+            faceBmp = ImageSaveUtil.loadBitmapFromPath(this, facePath);
+            if (faceBmp != null) {
+                faceIv.setImageBitmap(faceBmp);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(faceBmp != null) {
+            faceBmp.recycle();
         }
     }
 
@@ -178,13 +224,13 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
     private long getMeetingTime() {
         long time = 0;
         Log.d(TAG, "date = " + dateSet + ", time = " + timeSet);
-        if((dateSet != null) && (timeSet != null)) {
+        if ((dateSet != null) && (timeSet != null)) {
             datetime = new StringBuffer(dateSet).append(" ").append(timeSet);
             Log.d(TAG, "date + time " + datetime.toString());
             try {
                 time = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA).parse(datetime.toString()).getTime();
                 Log.d(TAG, "getMeetingTime Time = " + time);
-            }catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -202,7 +248,7 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
                 tv_date.setText(DateFormat.format("yyyy-MM-dd", mCalendar));
                 dateSet = DateFormat.format("yyyy-MM-dd", mCalendar).toString();
             }
-        },mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH));
+        }, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.setMessage("请选择日期");
         datePickerDialog.show();
     }
@@ -237,10 +283,10 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
     }
 
     private void updateDateTimeLL(boolean meetingPassed) {
-        if(meetingPassed) {
+        if (meetingPassed) {
             mSetMeetingTimell.setVisibility(View.VISIBLE);
             mMeetingTimell.setVisibility(View.GONE);
-        }else {
+        } else {
             mSetMeetingTimell.setVisibility(View.GONE);
             mMeetingTimell.setVisibility(View.VISIBLE);
         }
@@ -248,8 +294,8 @@ public class MeetingRegActivity extends BaseActivity<IMeetingView, MeetingPresen
 
     private void registerMeetingInfo() {
         Log.d(TAG, "registerMeetingInfo: name = " + etMeetingName.getText().toString() +
-        ", time = " + datetime.toString() + ", addr = " + etMeetingAddr.getText().toString() +
-        ", part = " + etParticipantPart.getText().toString() + ", username = " + etParticipantName.getText().toString());
+                ", time = " + datetime.toString() + ", addr = " + etMeetingAddr.getText().toString() +
+                ", part = " + etParticipantPart.getText().toString() + ", username = " + etParticipantName.getText().toString());
         if ((etMeetingName != null) && (etMeetingTime != null) && (etMeetingAddr != null) &&
                 (etParticipantPart != null) && (etParticipantName != null)) {
             mMeetingFace.setMeetingName(etMeetingName.getText().toString());
--- app/src/main/java/android/com/aiface/ui/base/BaseActivity.java
+++ app/src/main/java/android/com/aiface/ui/base/BaseActivity.java
@@ -1,18 +1,36 @@
 package android.com.aiface.ui.base;
 
+import android.Manifest;
+import android.com.aiface.baidu.APIService;
+import android.com.aiface.baidu.exception.FaceError;
+import android.com.aiface.baidu.utils.Md5;
+import android.com.aiface.baidu.utils.OnResultListener;
 import android.com.aiface.database.GreenDaoManager;
 import android.com.aiface.settings.SettingManager;
+import android.com.aiface.ui.activity.FaceDetectActivity;
 import android.com.aiface.utils.ToastMsg;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+
+import java.io.File;
 
 public abstract class BaseActivity<V, T extends BasePresenter<V>> extends AppCompatActivity {
     protected T mPresenter;
     protected SettingManager mSettingManager;
     protected GreenDaoManager greenDaoManager;
     protected ToastMsg mToastInstance;
+
+    protected static final int REQUEST_CODE_DETECT_FACE = 1000;
+    protected static final int REQUEST_CODE_PICK_IMAGE = 1001;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,5 +77,85 @@ public abstract class BaseActivity<V, T extends BasePresenter<V>> extends AppCom
     public void jumpToActivity(Class activity) {
         Intent intent = new Intent(this, activity);
         startActivity(intent);
+    }
+
+    public void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+            return;
+        }
+    }
+
+    public void uploadFromAlbum() {
+        checkPermission();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    public void uploadFromCamera() {
+        checkPermission();
+        Intent intent = new Intent(this, FaceDetectActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_DETECT_FACE);
+    }
+
+    public String getFacePathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+
+    public void registerFaceImage(String facePath, final String userName) {
+        if(TextUtils.isEmpty(userName)) {
+            mToastInstance.showShortToast("姓名不能为空");
+            return;
+        }
+
+        if(TextUtils.isEmpty(facePath)) {
+            mToastInstance.showShortToast("图像不能为空");
+            return;
+        }
+
+        final File file = new File(facePath);
+        if(!file.exists()) {
+            mToastInstance.showShortToast("文件不存在");
+            return;
+        }
+
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                RegisterFaceToBaidu(file, userName);
+            }
+        });
+    }
+
+    private void RegisterFaceToBaidu(File file, String userName) {
+        String userId = Md5.MD5(userName, "utf-8");
+        APIService.getInstance().reg(new OnResultListener() {
+            @Override
+            public void onResult(Object result) {
+                mToastInstance.showShortToast("注册成功");
+            }
+
+            @Override
+            public void onError(FaceError error) {
+                mToastInstance.showShortToast("注册失败");
+            }
+        }, file, userId, userName);
     }
 }
